16f2f9fa0cf663360de0832881b25759e08f6e9c
Abhishek Bhakari <130546310+Abhishekbhakari@users.noreply.github.com>
2023-12-17 14:20:49 +0530

    Update SignIn.js

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -15,7 +15,7 @@ function SignIn() {
     try {
       const response = await axios({
         method: "post",
-        url: URL + "/api/auth/signin",
+        url: URL + "https://full-stack-authintication-system.vercel.app/signin",
         withCredentials: true,
         data: credentials
       });
